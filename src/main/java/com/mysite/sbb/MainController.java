c43a59bde68c545892facc1db52f7aa517c3d9e0
kk0506014 <kk0506014@gmail.com>
2024-12-10 11:44:34 +0900

    touch db

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,13 +2,16 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     // GET http://localhost:8080/
     @GetMapping("/")
-    public void home() {
-        System.out.println("home");
+    @ResponseBody
+    public String home() {
+        System.out.println("안녕하세요.");
+        return "안녕하세요.";
     }
 
     // GET http://localhost:8080/about
