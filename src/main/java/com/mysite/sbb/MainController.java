db0a38b3d5e4ea68a90fa9f9a183e92f31028acd
kk0506014 <kk0506014@gmail.com>
2024-12-11 09:25:23 +0900

    sbb Question update

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -6,17 +6,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    // GET http://localhost:8080/
     @GetMapping("/")
     @ResponseBody
-    public String home() {
-        System.out.println("안녕하세요.");
+    public int home() {
+        System.out.println("home");
+        return 22;
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about() {
+        System.out.println("about");
         return "안녕하세요.";
     }
 
-    // GET http://localhost:8080/about
-    @GetMapping("/about")
-    public void about() {
-        System.out.println("about");
+    public void contact() {
+        System.out.println("contact");
     }
 }
